796a165e93be893b6f3630a746224f242c297d88
SachinGadge13 <gadges14@gmail.com>
2022-09-01 22:34:07 +0530

    added subtraction V-3

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,10 @@ public class Calculator {
 		int num1=10;
 		int num2=20;
 		int sum=num1+num2;
+		int sub=num1-num2;
 		System.out.println("Sum is: "+ sum);
+		System.out.println("Sub is: "+ sub);
+
 
 	}
 
