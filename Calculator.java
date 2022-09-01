8cba327cd5652a24dad4b615f5dcf77c58d339d0
SachinGadge13 <gadges14@gmail.com>
2022-09-01 22:41:42 +0530

    added division

--- Calculator.java
+++ Calculator.java
@@ -9,9 +9,12 @@ public class Calculator {
 		int sum=num1+num2;
 		int sub=num1-num2;
 		int mul=num1*num2;
+		int div=num1/num2;
 		System.out.println("Sum is: "+ sum);
 		System.out.println("Sub is: "+ sub);
 		System.out.println("mul is: "+ mul);
+		System.out.println("div is: "+ div);
+
 
 
 	}
