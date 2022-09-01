2549624009fe8cab96aae261b65f42537eb5f803
SachinGadge13 <gadges14@gmail.com>
2022-09-01 22:26:00 +0530

    Added- addition file V-2

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,10 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Calculator!!!");
+		int num1=10;
+		int num2=20;
+		int sum=num1+num2;
+		System.out.println("Sum is: "+ sum);
 
 	}
 
