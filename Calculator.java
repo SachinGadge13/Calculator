1765bffd84790141d1c7ca6a0a7ba968a0f34352
SachinGadge13 <gadges14@gmail.com>
2022-09-01 22:38:32 +0530

    added Multiplication

--- Calculator.java
+++ Calculator.java
@@ -8,8 +8,10 @@ public class Calculator {
 		int num2=20;
 		int sum=num1+num2;
 		int sub=num1-num2;
+		int mul=num1*num2;
 		System.out.println("Sum is: "+ sum);
 		System.out.println("Sub is: "+ sub);
+		System.out.println("mul is: "+ mul);
 
 
 	}
